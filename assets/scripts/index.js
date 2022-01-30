df291ca4ced05bc042da30304e9fcb93b30ef8aa
redoverflow <punctuwuation@gmail.com>
2022-01-30 23:18:05 +0300

    woaaa channel modiyfing !!!!!

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -20,6 +20,7 @@ function convertMidiToSequence (midi) {
 let data;
 let ok = true;
 var soundlist;
+var soundnames = [];
 try {
   await fetch("https://gdcolon.com/server/soundlist")
   .then(response => response.json())
@@ -32,39 +33,31 @@ try {
 if (ok) {
   soundlist = data;
   console.log(soundlist);
-  let soundnames = [];
   for (let i = 0; i < soundlist.length; i++) {
-    soundnames.push([soundlist[i]["name"], "https://gdcolon.com/"+soundlist[i]["image"]]);
+    soundnames.push(soundlist[i]["name"]);
   }
-}
-//iterate over soundlist and get all of the sound names
-var soundnames = [];
-for (let i = 0; i < soundlist.length; i++) {
-    soundnames.push([soundlist[i]["name"], "https://gdcolon.com/"+soundlist[i]["image"]]);
 }
 
 var soundopts = "";
 for (let i = 0; i < soundnames.length; i++) {
-    soundopts += "<option value='"+soundnames[i][0]+"'>"+soundnames[i][0]+"</option>\n";
+    soundopts += "<option value='"+soundnames[i]+"'>"+soundnames[i]+"</option>";
 }
 
-window.onload = function() {
-    var midifilein = document.getElementById("midifile");
-    midifilein.addEventListener('change', function(e) {
-        var midifilein1 = midifilein.files[0];
-        var reader = new FileReader();
-        reader.onload = function(e) {
-            const midi = new Midi(e.target.result);
-            //console.log(researchMidi(midi));
+var midifilein = document.getElementById("midifile");
+midifilein.addEventListener('change', function(e) {
+    var midifilein1 = midifilein.files[0];
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        const midi = new Midi(e.target.result);
+        //console.log(researchMidi(midi));
 
-            //put channel # instrument choices in the page
-            var chinsts = document.getElementById("chinsts");
-            for (let i = 0; i < midi.tracks.length; i++) {
-                chinsts.innerHTML += `<label for="ch1inst" class="form-label">Channel 1 instrument</label>
-                <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">`+soundopts+`</select>`;
-            }
+        //put channel # instrument choices in the page
+        var chinsts = document.getElementById("chinsts");
+        for (let i = 0; i < midi.tracks.length; i++) {
+            chinsts.innerHTML += `<label for="ch1inst" class="form-label">Channel `+(i+1).toString()+` instrument</label>
+            <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">`+soundopts+`</select>`;
         }
-        reader.readAsArrayBuffer(midifilein1);
-    });
-}
+    }
+    reader.readAsArrayBuffer(midifilein1);
+});
 })();
