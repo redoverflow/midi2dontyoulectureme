f47003df3b333f148e8719616235b9d16c9d881d
redoverflow <punctuwuation@gmail.com>
2022-01-31 12:57:02 +0300

    IM GONNA HIT MY HEAD AGAINST THE FUCKIG NWAL AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -15,7 +15,7 @@ function researchMidi (midi) {
 
 function convertMidiToSequence (midi, singulartrack, track) {
     var outseq = "";
-    outseq += "!speed@" + researchMidi(midi)[0]*5 + "|";
+    outseq += "!speed@" + researchMidi(midi)[0]*10 + "|";
     notescheatsheet = {
         "C": 0,
         "D": 1,
@@ -39,7 +39,9 @@ function convertMidiToSequence (midi, singulartrack, track) {
                 note = note.replace("#", "");
             }
             let octave = Number(note.substring(1));
-            if (octave > 5) {
+            if (octave == 5) {
+                //no pitching needed
+            } else if (octave > 5) {
                 pitch += 12*(octave-5);
             } else if (octave < 5) {
                 pitch -= 12*(5-octave);
@@ -48,8 +50,7 @@ function convertMidiToSequence (midi, singulartrack, track) {
             //in miliseconds
             let duration = Math.round(midi["tracks"][track-1]["notes"][i]["duration"]*1000);
             //in beats
-            //junk code alert!!!
-            duration = roundNumber(duration/midi["header"]["tempos"][0]["bpm"], Number(Math.round(duration/midi["header"]["tempos"][0]["bpm"]).toString().length)+1);
+            duration = roundNumber(duration/researchMidi(midi)[0], Number(Math.round(duration/researchMidi(midi)[0]).toString().length)+1);
             //velocity
             let velocity = Math.round(midi["tracks"][track-1]["notes"][i]["velocity"]*100);
 
@@ -102,6 +103,7 @@ midifilein.addEventListener('change', function(e) {
 
         //put channel # instrument choices in the page
         var chinsts = document.getElementById("chinsts");
+        chinsts.innerHTML = "";
         for (let i = 0; i < midi.tracks.length; i++) {
             chinsts.innerHTML += `<label for="chinst`+(midi["tracks"][i]["channel"]+1).toString()+`" class="form-label">Channel `+(midi["tracks"][i]["channel"]+1).toString()+` instrument</label>
             <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" id=\"chinst`+(midi["tracks"][i]["channel"]+1).toString()+`\">`+soundopts+`</select>`;
@@ -121,10 +123,7 @@ convertbutton.addEventListener('click', function(e) {
     let radiochecked = 0;
     for (var i = 0, length = radios.length; i < length; i++) {
         if (radios[i].checked) {
-          // do whatever you want with the checked radio
           radiochecked = radios[i].value;
-      
-          // only one radio can be logically checked, don't check the rest
           break;
         }
       }
